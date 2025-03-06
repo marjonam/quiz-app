cdda42970709cd074e71b9f1342a97b805c73e60
marjona@gmail.com <marjona@gmail.com>
2025-03-06 22:06:21 +0300

    .

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,30 +1,53 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  data: null,
+  isPending: false,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOADING":
+      return { ...state, isPending: true, error: null };
+    case "SUCCESS":
+      return { data: action.payload, isPending: false, error: null };
+    case "ERROR":
+      return { data: null, isPending: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export function useFetch(url) {
-  const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsPending(true);
-      try {
-        const req = await fetch(url);
-        console.log(req);
-        if (!req.ok) {
-          throw new Error(req.statusText);
+    let isMounted = true;
+    dispatch({ type: "LOADING" });
+
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.statusText);
         }
-        const data = await req.json();
-        setData(data);
-        setIsPending(false);
-      } catch (err) {
-        setError(err.message);
-        console.log(err.message);
-        setIsPending(false);
-      }
+        return res.json();
+      })
+      .then((data) => {
+        if (isMounted) {
+          dispatch({ type: "SUCCESS", payload: data });
+        }
+      })
+      .catch((error) => {
+        if (isMounted) {
+          dispatch({ type: "ERROR", payload: error.message });
+        }
+      });
+
+    return () => {
+      isMounted = false;
     };
-    fetchData();
   }, [url]);
 
-  return { data, isPending, error };
+  return state;
 }
